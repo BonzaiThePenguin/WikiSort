6b1df8dbea585531034ee23464de14604456d2e6
BonzaiThePenguin <mike@bonzaigames.com>
2014-02-22 19:11:06 -0500

    Update bzSort.c

--- bzSort.c
+++ bzSort.c
@@ -3,7 +3,11 @@
 #include <stdint.h>
 #include <string.h>
 
-uint64_t floor_power_of_two(uint64_t x) {
+#define is_even(value) ((value & 0x1) == 0x0)
+typedef uint64_t uint64; // i hate the _t
+
+// 63 -> 32, 64 -> 64, etc.
+uint64 floor_power_of_two(uint64 x) {
    x |= (x >> 1); x |= (x >> 2); x |= (x >> 4);
    x |= (x >> 8); x |= (x >> 16); x |= (x >> 32);
    x -= (x >> 1) & 0x5555555555555555;
@@ -16,47 +20,52 @@ uint64_t floor_power_of_two(uint64_t x) {
 // this assumes that if a <= b and b <= c, then a <= c
 #define bzSort(array, array_count, compare) { \
    __typeof__(array[0]) temp, *bzSort_array = array; const long bzSort_count = array_count; \
-   uint64_t i; \
+   uint64 i; \
    if (bzSort_count < 32) { \
       /* insertion sort the array */ \
       for (i = 1; i < bzSort_count; i++) { \
-         temp = bzSort_array[i]; uint64_t j = i; \
+         temp = bzSort_array[i]; uint64 j = i; \
          while (j > 0 && compare(bzSort_array[j - 1], temp) > 0) { bzSort_array[j] = bzSort_array[j - 1]; j--; } \
          bzSort_array[j] = temp; \
       } \
    } else { \
-      const uint64_t swap_size = 1024; /* change this as desired */ \
+      const uint64 swap_size = 1024; /* change this as desired */ \
       __typeof__(bzSort_array[0]) bzSort_swap[swap_size]; \
       /* calculate how to scale the index value to the range within the array */ \
-      uint64_t pot = floor_power_of_two(bzSort_count); \
+      uint64 pot = floor_power_of_two(bzSort_count); \
       double scale = bzSort_count/(double)pot; /* 1.0 <= scale < 2.0 */ \
       /* multiplying by scale below is proven to be correct for more than 17,179,869,184 elements */ \
-      uint64_t index = 0, start, mid, end, iteration, merge, length; \
+      uint64 index = 0, start, mid, end, iteration, merge, length; \
       while (index < pot) { \
          start = index * scale; mid = (index + 16) * scale; end = (index + 32) * scale; \
          /* insertion sort from start to mid and mid to end */ \
          for (i = start + 1; i < mid; i++) { \
-            temp = bzSort_array[i]; uint64_t j = i; \
+            temp = bzSort_array[i]; uint64 j = i; \
             while (j > start && compare(bzSort_array[j - 1], temp) > 0) { bzSort_array[j] = bzSort_array[j - 1]; j--; } \
             bzSort_array[j] = temp; \
          } \
          for (i = mid + 1; i < end; i++) { \
-            temp = bzSort_array[i]; uint64_t j = i; \
+            temp = bzSort_array[i]; uint64 j = i; \
             while (j > mid && compare(bzSort_array[j - 1], temp) > 0) { bzSort_array[j] = bzSort_array[j - 1]; j--; } \
             bzSort_array[j] = temp; \
          } \
          merge = index; index += 32; iteration = index/16; length = 16; \
          while ((iteration & 0x1) == 0x0) { \
-            start = merge * scale; mid = (merge + length) * scale; end = (merge + length + length) * scale; /* scale these ranges */ \
-            if (compare(bzSort_array[mid - 1], bzSort_array[mid]) > 0) { /* don't merge if they're already in order */ \
+            start = merge * scale; mid = (merge + length) * scale; end = (merge + length + length) * scale; \
+            /* don't merge if they're already in order, like so: 0 1 2 3 | 4 5 6 7 */ \
+            /* ints[mid - 1] = 3 and ints[mid] = 4, and 3 <= 4, so the code below is skipped */ \
+            if (compare(bzSort_array[mid - 1], bzSort_array[mid]) > 0) { \
+               /* see if the two segments are in the wrong order, like in this example: 4 5 6 7 | 0 1 2 3 */ \
+               /* ints[start] = 4 and ints[end - 1] = 3, and 4 > 3, so we only need to swap the segments rather than perform a full merge */ \
                if (compare(bzSort_array[start], bzSort_array[end - 1]) > 0) { \
                   /* the size of the two sides will never differ by more than 1, so we can just have a separate swap here for a single variable */ \
                   if (mid - start >= end - mid) { \
-                     uint64_t a_from = start, a_to = mid, b_from = mid, b_to = start, count = end - mid; \
+                     /* 3 4 5 6 | 0 1 2... the left side has one more item (or they are the same size) */ \
+                     uint64 a_from = start, a_to = mid, b_from = mid, b_to = start, count = end - mid; \
                      if (mid - start != end - mid) temp = bzSort_array[a_to = mid - 1]; \
                      while (count > 0) { \
                         /* copy values from the left side into swap */ \
-                        uint64_t read = (swap_size < count) ? swap_size : count; \
+                        uint64 read = (swap_size < count) ? swap_size : count; \
                         memcpy(&bzSort_swap[0], &bzSort_array[a_from], read * sizeof(bzSort_array[0])); \
                         memmove(&bzSort_array[b_to], &bzSort_array[b_from], read * sizeof(bzSort_array[0])); \
                         memcpy(&bzSort_array[a_to], &bzSort_swap[0], read * sizeof(bzSort_array[0])); \
@@ -64,11 +73,12 @@ uint64_t floor_power_of_two(uint64_t x) {
                      } \
                      if (mid - start != end - mid) bzSort_array[end - 1] = temp; \
                   } else { \
-                     uint64_t a_from = end, a_to = mid, b_from = mid, b_to = end, count = mid - start; \
+                     /* 4 5 6 | 0 1 2 3... the right side has one more item */ \
+                     uint64 a_from = end, a_to = mid, b_from = mid, b_to = end, count = mid - start; \
                      if (mid - start != end - mid) { temp = bzSort_array[mid]; a_to++; } \
                      while (count > 0) { \
-                        /* copy values from the left side into swap */ \
-                        uint64_t read = (swap_size < count) ? swap_size : count; \
+                        /* copy values from the right side into swap */ \
+                        uint64 read = (swap_size < count) ? swap_size : count; \
                         a_from -= read; a_to -= read; b_from -= read; b_to -= read; count -= read; \
                         memcpy(&bzSort_swap[0], &bzSort_array[a_from], read * sizeof(bzSort_array[0])); \
                         memmove(&bzSort_array[b_to], &bzSort_array[b_from], read * sizeof(bzSort_array[0])); \
@@ -77,8 +87,9 @@ uint64_t floor_power_of_two(uint64_t x) {
                      if (mid - start != end - mid) bzSort_array[start] = temp; \
                   } \
                } else { \
-                  /* add the smaller of the two values to swap */ \
-                  uint64_t insert = 0, count = 0, index1 = start, index2 = mid, swap_to = start, swap_from = 0; \
+                  /* standard merge operation. add the smaller of the two values to swap, */ \
+                  /* then copy the values back to the array if swap runs out of space. */ \
+                  uint64 insert = 0, count = 0, index1 = start, index2 = mid, swap_to = start, swap_from = 0; \
                   while (index1 < mid && index2 < end) { \
                      count++; bzSort_swap[insert++] = (compare(bzSort_array[index1], bzSort_array[index2]) <= 0) ? bzSort_array[index1++] : bzSort_array[index2++]; \
                      if (insert >= swap_size) insert = 0; \
